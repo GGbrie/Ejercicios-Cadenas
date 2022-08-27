70937de1ef9cb30425579b23dfd8b5134ffbb184
GGbrie <106721167+GGbrie@users.noreply.github.com>
2022-08-26 18:51:45 -0600

    Delete ejercicio10.py

--- ejercicio10.py
+++
@@ -1,7 +0,0 @@
-Ejercicio 10
-Escribir un programa que pregunte por consola por los productos de una cesta de la compra, separados por comas, y muestre por pantalla cada uno de los productos en una línea distinta.
-
-------------------------------------------------------
-
-cesta = input('Que productos en la canasta compro: ')
-print(cesta.replace(',', '\n'))
